bea5c67d8b139801251dbd8b626aaab367dd5210
Joseph Sebastian <subins@gmail.com>
2018-01-08 11:21:51 +0000

    Appln added

---
+++ src/main/scala/AkkaHttpMicroservice.scala
@@ -0,0 +1,21 @@
+import akka.actor.ActorSystem
+import akka.event.{LoggingAdapter, Logging}
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import java.io.IOException
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.math._
+import spray.json.DefaultJsonProtocol
+
+object AkkaHttpMicroservice extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorMaterializer()
+
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+}
