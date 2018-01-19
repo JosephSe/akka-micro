f9aa1814cd1eadffd7e0c151b93902b1dd725025
Joseph Sebastian <subins@gmail.com>
2018-01-19 13:54:36 +0000

    route impl

--- src/main/scala/AkkaHttpMicroservice.scala
+++ src/main/scala/AkkaHttpMicroservice.scala
@@ -1,5 +1,13 @@
 import akka.actor.ActorSystem
 import akka.event.{LoggingAdapter, Logging}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.marshalling.ToResponseMarshallable
+import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.{ActorMaterializer, Materializer}
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.typesafe.config.Config
@@ -11,17 +19,28 @@ import spray.json.DefaultJsonProtocol
 
 case class IpInfo(query: String, country: Option[String], city: Option[String], lat: Option[Double], lon: Option[Double])
 
+case class IpPairSummaryRequest(ip1: String, ip2: String)
+
 case class IpPairSummary(distance: Option[Double], ip1Info: IpInfo, ip2Info: IpInfo)
 
-object AkkaHttpMicroservice extends App with Service {
-  override implicit val system = ActorSystem()
-  override implicit val executor = system.dispatcher
-  override implicit val materializer = ActorMaterializer()
+object IpPairSummary {
+  def apply(ip1Info: IpInfo, ip2Info: IpInfo): IpPairSummary = IpPairSummary(calculateDistance(ip1Info, ip2Info), ip1Info, ip2Info)
 
-  override val config = ConfigFactory.load()
-  override val logger = Logging(system, getClass)
+  private def calculateDistance(ip1Info: IpInfo, ip2Info: IpInfo): Option[Double] = {
+    (ip1Info.lat, ip1Info.lon, ip2Info.lat, ip2Info.lon) match {
+	// TODO implement logic
+        Option(EarthRadius)
+      case _ => None
+    }
+  }
 
-  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+  private val EarthRadius = 6371.0
+}
+
+trait Protocols extends DefaultJsonProtocol {
+  implicit val ipInfoFormat = jsonFormat5(IpInfo.apply)
+  implicit val ipPairSummaryRequestFormat = jsonFormat2(IpPairSummaryRequest.apply)
+  implicit val ipPairSummaryFormat = jsonFormat3(IpPairSummary.apply)
 }
 
 trait Service extends Protocols {
@@ -36,6 +55,20 @@ trait Service extends Protocols {
     Http().outgoingConnection(config.getString("services.ip-api.host"), config.getInt("services.ip-api.port"))
 
   def ipApiRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request).via(ipApiConnectionFlow).runWith(Sink.head)
+
+  def fetchIpInfo(ip: String): Future[Either[String, IpInfo]] = {
+    ipApiRequest(RequestBuilding.Get(s"/json/$ip")).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[IpInfo].map(Right(_))
+        case BadRequest => Future.successful(Left(s"$ip: incorrect IP format"))
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          val error = s"FreeGeoIP request failed with status code ${response.status} and entity $entity"
+          logger.error(error)
+          Future.failed(new IOException(error))
+        }
+      }
+    }
+  }
 
   val routes = {
     logRequestResult("akka-http-microservice") {
@@ -62,5 +95,15 @@ trait Service extends Protocols {
       }
     }
   }
+}
 
+object AkkaHttpMicroservice extends App with Service {
+  override implicit val system = ActorSystem()
+  override implicit val executor = system.dispatcher
+  override implicit val materializer = ActorMaterializer()
+
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
+
+  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
 }
